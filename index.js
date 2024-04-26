e5785882af22975000df1584b2f391a76c718d80
manutommmmmmmmmmmmmmmmmm <134299193+manutommmmmmmmmmmmmmmmmm@users.noreply.github.com>
2024-04-26 10:19:09 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-let passwordLength = document.getElementById("passwordLength")
-let password = document.getElementById("password")
+let passwordLength = document.getElementById("passwordLength");
+let password = document.getElementById("password");
+let saveButton = document.getElementById("saveButton");
 
 function generatePassword(length) {
-    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
-    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    const numeric = "0123456789"
-    const Symbol = "`!@#$%^&*()_+|}{:?></.,';\\][~";
+    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numeric = "0123456789";
+    const symbols = "`!@#$%^&*()_+|}{:?></.,';\\][~";
 
-    const data = lowerAlphabet + upperAlphabet + numeric + Symbol;
+    const data = lowerAlphabet + upperAlphabet + numeric + symbols;
     let generator = '';
     for (let index = 0; index < length; index++) {
-        generator += data[~~(Math.random() * data.length)]; // fixed += to concatenate characters
+        generator += data[~~(Math.random() * data.length)];
     }
-    return generator
+    return generator;
 }
 
 function getPassword() {
-    const newPassword = generatePassword(passwordLength.value)
-    password.value = newPassword
-    setTimeout(() => {
-        alert('password has been generated!!')
-    }, 200)
+    const newPassword = generatePassword(passwordLength.value);
+    password.value = newPassword;
+    alert('Kata sandi telah dihasilkan!!');
 }
 
 function savePassword() {
+    const newPassword = password.value.trim();
+
+    if (newPassword === '') {
+        alert('Kata sandi tidak boleh kosong.');
+        return;
+    }
+
+    document.title = newPassword;
+
+    // Membuat tautan untuk mengunduh kata sandi sebagai file teks
+    const downloadLink = document.createElement('a');
+    downloadLink.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`password saya: ${newPassword}`));
+    downloadLink.setAttribute('download', 'MyPasswordGenerateLOG.txt');
+    downloadLink.style.display = 'none';
+    document.body.appendChild(downloadLink);
+
+    // Mengklik tautan unduh secara otomatis
+    downloadLink.click();
+
+    // Hapus tautan unduh dari DOM setelah selesai
+    document.body.removeChild(downloadLink);
+
+    alert('Kata sandi berhasil disimpan.');
 }
